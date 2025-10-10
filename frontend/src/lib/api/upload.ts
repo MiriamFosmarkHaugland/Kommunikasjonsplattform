49f6925f3dd59b8137f0b31df7f10f99e98d067a
Miriam F. Haugland <mifo007@student.kristiania.no>
2025-10-10 13:52:13 +0200

    Removed try-catch and renamed url variable.

--- frontend/src/lib/api/upload.ts
+++ frontend/src/lib/api/upload.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const ImageUploadEndpoint = "http://localhost:5041/api/UploadImage";
+const API_BASE_URL = "http://localhost:5041/api/UploadImage";
 
 export default async function uploadImage(image: File) {
     const formData = new FormData();
     formData.append("file", image);
 
     const response = await axios({
-        url: ImageUploadEndpoint,
+        url: API_BASE_URL,
         method: "POST",
         data: formData,
         headers: {
--- frontend/src/lib/api/user.ts
+++ frontend/src/lib/api/user.ts
@@ -1,36 +1,25 @@
 import axios from "axios";
 import { User } from "../types/user";
 
-const BASE_URL = "http://localhost:5041/api/user"
+const API_BASE_URL = "http://localhost:5041/api/user"
 
 export async function addUser(user: Partial<User>) {
-    try {
-        const response = await axios.post(BASE_URL, user, {
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-    } catch (error) {
-        throw error
-    }
+    const response = await axios.post(API_BASE_URL, user, {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
 }
 
+
 export async function updateUser(id: string, user: Partial<User>){
-    try {
-        const response = await axios.patch(`${BASE_URL}/${id}`, user, {
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-    } catch (error) {
-        throw error
-    }
+    const response = await axios.patch(`${API_BASE_URL}/${id}`, user, {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
 }
 
 export async function deleteUser(id: string) {
-    try {
-        const response = await axios.delete(`${BASE_URL}/${id}`);
-    } catch (error) {
-        throw error
-    }
+    const response = await axios.delete(`${API_BASE_URL}/${id}`);
 }
